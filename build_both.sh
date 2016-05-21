ba7103df40cd25dfb621468cb54c672997945578
Ben Meier <benmeier@fastmail.com>
2016-05-21 15:27:05 +0200

    Added docker demo

---
+++ build_both.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+function buildbinary {
+    goos=$1
+    goarch=$2
+
+    echo "Building $goos $goarch binary"
+
+    outputfolder="build/${goos}_${goarch}"
+    echo "Output Folder $outputfolder"
+    mkdir -pv $outputfolder
+
+    export GOOS=$goos
+    export GOARCH=$goarch
+
+    go build -i -v -o "$outputfolder/rpc-cmd-client" github.com/AstromechZA/middleman/client
+    go build -i -v -o "$outputfolder/rpc-cmd-server" github.com/AstromechZA/middleman/server
+
+    echo "Done"
+    ls -l "$outputfolder"
+    echo
+}
+
+# build for mac
+buildbinary darwin amd64
+
+# build for linux
+buildbinary linux amd64
---
+++ docker-demo/run-demo.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "Building binaries.."
+export GOOS=linux
+export GOARCH=amd64
+go build -i -v -o "$DIR/client-container/rpc-cmd-client" github.com/AstromechZA/middleman/client
+go build -i -v -o "$DIR/server-container/rpc-cmd-server" github.com/AstromechZA/middleman/server
+
+echo "Building containers.."
+docker build -t rpc-cmd-server-container "$DIR/server-container"
+docker build -t rpc-cmd-client-container "$DIR/client-container"
+
+echo ""
+echo "Launching server.."
+docker run -d --name rcs-container rpc-cmd-server-container
+echo ""
+echo "Launching client.."
+docker run --rm --volumes-from rcs-container rpc-cmd-client-container || true
+echo ""
+echo "Stopping server"
+docker stop rcs-container
+echo ""
+echo "Removing server"
+docker rm rcs-container
